0ee347a871b7e2530a84cf2449a957ef3321dc6c
njerucyrus <njerucyrus5@hotmail.com>
2017-03-15 01:09:29 +0300

    added auth

---
+++ models/class.auth.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudutech
+ * Date: 3/15/17
+ * Time: 1:02 AM
+ */
+
+require_once '../db/class.db.php';
+
+class Auth{
+
+    public function authenticate($username, $password)
+    {
+        global $conn;
+        try {
+            $stmt = $conn->prepare("SELECT * FROM users WHERE paypal_email=:username");
+            $stmt->bindParam(":username", $username);
+
+            $stmt->execute();
+
+            if ($stmt->rowCount() == 1) {
+
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                if (password_verify($password, $row['password'])) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            } else {
+                return false;
+            }
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+}
