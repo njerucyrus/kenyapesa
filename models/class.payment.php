9d6c8ca935ef3a8ef03f8183b0a6322a6a7b1cfe
njerucyrus <njerucyrus5@hotmail.com>
2017-03-15 16:37:48 +0300

    added payment and rates calc

--- models/class.payment.php
+++ models/class.payment.php
@@ -8,31 +8,272 @@
 require_once 'interface.crud.php';
 require_once '../db/class.db.php';
 
-class Payment implements PesaCrud {
+class Payment implements PesaCrud
+{
+
+    private $userId;
+    private $paypalEmail;
+    private $transactionId;
+    private $dollars;
+    private $shilling;
+    private $itemId;
+    private $status;
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param mixed $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPaypalEmail()
+    {
+        return $this->paypalEmail;
+    }
+
+    /**
+     * @param mixed $paypalEmail
+     */
+    public function setPaypalEmail($paypalEmail)
+    {
+        $this->paypalEmail = $paypalEmail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * @param mixed $transactionId
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = $transactionId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDollars()
+    {
+        return $this->dollars;
+    }
+
+    /**
+     * @param mixed $dollars
+     */
+    public function setDollars($dollars)
+    {
+        $this->dollars = $dollars;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShilling()
+    {
+        return $this->shilling;
+    }
+
+    /**
+     * @param mixed $shilling
+     */
+    public function setShilling($shilling)
+    {
+        $this->shilling = $shilling;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * @param mixed $itemId
+     */
+    public function setItemId($itemId)
+    {
+        $this->itemId = $itemId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
 
     public function create()
     {
-        // TODO: Implement create() method.
+        global $conn;
+
+        $userId = $this->getUserId();
+        $paypalEmail = $this->getPaypalEmail();
+        $transactionId = $this->getTransactionId();
+        $dollars = $this->getDollars();
+        $shillings = $this->getShilling();
+        $itemId = $this->getItemId();
+        $status = $this->getStatus();
+
+        try {
+
+            $stmt = $conn->query("INSERT INTO payments(transaction_id, item_id, user_id, paypal_email, dollars, shillings, status)
+                                  VALUES (:transaction_id, :item_id, :user_id, :paypal_email, :dollars, :shillings, :status)");
+
+            $stmt->bindParam(":transaction_id", $transactionId);
+            $stmt->bindParam(":item_id", $itemId);
+            $stmt->bindParam(":user_id", $userId);
+            $stmt->bindParam(":paypal_email", $paypalEmail);
+            $stmt->bindParam(":dollars", $dollars);
+            $stmt->bindParam(":shillings", $shillings);
+            $stmt->bindParam(":status", $status);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+
+            print_r(json_encode(array(
+                'statusCode' => 500,
+                'message' => "Error " . $e->getMessage()
+            )));
+            return false;
+        }
+
     }
 
-    public function update($id)
+    /**
+     * @param $transactionId
+     * @return bool
+     * updated the transaction status
+     * after payment has been completed successfully
+     */
+    public  function update($transactionId)
     {
-        // TODO: Implement update() method.
+        global $conn;
+        $status = $this->getStatus();
+        try {
+
+            $stmt = $conn->query("UPDATE payments SET status=:status
+                                  WHERE transaction_id=:transaction_id");
+
+            $stmt->bindParam(":transaction_id", $transactionId);
+            $stmt->bindParam(":status", $status);
+            $stmt->execute();
+            return true;
+
+        } catch (PDOException $e) {
+
+            print_r(json_encode(array(
+                'statusCode' => 500,
+                'message' => "Error " . $e->getMessage()
+            )));
+            return false;
+        }
     }
 
     public static function delete($id)
     {
-        // TODO: Implement delete() method.
+        global $conn;
+
+        try {
+            $stmt = $conn->query("DELETE FROM payments WHERE id=:id");
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            return true;
+        } catch (PDOException $e) {
+
+            print_r(json_encode(array(
+                'statusCode' => 500,
+                'message' => "Error " . $e->getMessage()
+            )));
+            return false;
+        }
+
     }
 
+    /**
+     * @param $id
+     * @return null|PDOStatement
+     */
     public static function getById($id)
     {
-        // TODO: Implement getById() method.
+        global $conn;
+
+        try {
+
+            $stmt  = $conn->query("SELECT * FROM payments WHERE id=:id AND status='success'");
+
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0 ) {
+                return $stmt;
+            }
+            else{
+                return null;
+            }
+
+        } catch (PDOException $e) {
+            print_r(json_encode(array(
+                'statusCode' => 500,
+                'message' => "Error " . $e->getMessage()
+            )));
+            return null;
+        }
     }
 
+    /**
+     * @return null|PDOStatement
+     * S
+     */
     public static function all()
     {
-        // TODO: Implement all() method.
+        global $conn;
+
+        try {
+            $stmt = $conn->query("SELECT * FROM payments WHERE status='success'");
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                return $stmt;
+            } else {
+                return null;
+            }
+        } catch (PDOException $e) {
+
+            print_r(json_encode(array(
+                'statusCode' => 500,
+                'message' => "Error " . $e->getMessage()
+            )));
+            return null;
+        }
     }
 
 }
