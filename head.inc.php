d182f76c43fed94abd522173feb8ecf8166eb6ef
njerucyrus <njerucyrus5@hotmail.com>
2017-03-16 22:02:49 +0300

    changed btns

--- head.inc.php
+++ head.inc.php
@@ -13,7 +13,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-<!--        <!--Google Font link-->-->
+<!--        <!--Google Font link-->
 <!--        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">-->
 <!--        <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700" rel="stylesheet">-->
 
--- home.inc.php
+++ home.inc.php
@@ -9,24 +9,15 @@
 <div class="overlay">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12 ">
+            <div class="col-sm-12">
                 <div class="main_home_slider text-center">
                     <div class="single_home_slider">
                         <div class="main_home wow fadeInUp" data-wow-duration="700ms">
                             <h1>WELCOME TO OUR LOGIC</h1>
                             <p>We Make Awesome Theme For Your Business</p>
                             <div class="home_btn">
-                                <a href="" class="btn btn-primary" style="border-radius: 25px; margin: 2px;">SIGN UP</a>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="single_home_slider">
-                        <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                            <h1>WELCOME TO OUR LOGIC</h1>
-                            <p>We Make Awesome Theme For Your Business</p>
-                            <div class="home_btn">
-                                <a href="" class="btn btn-primary">LEARN MORE</a>
+                                <a href="#convert" class="btn btn-primary">Convert </a>
+                                <a href="#signup" class="btn btn-primary">Sign Up</a>
                             </div>
 
                         </div>
@@ -36,8 +27,8 @@
                             <h1>WELCOME TO KENYAPESA </h1>
                             <p>INSTANT PAYPAL TO MPESA WITHDRAW SERVICES IN KENYA</p>
                             <div class="home_btn">
-                                <a href="" class="btn btn-primary">Convert  </a>
-                                <a href="" class="btn btn-primary">Sign up</a>
+                                <a href="#convert" class="btn btn-primary">Convert </a>
+                                <a href="#signup" class="btn btn-primary">Sign Up</a>
                             </div>
 
                         </div>
