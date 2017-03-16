eb3cbd6299deb24aebc35ee7cf0bf66511c56925
bilihela <samgithae01@gmail.com>
2017-03-16 22:00:48 +0300

    added contact page

--- home.inc.php
+++ home.inc.php
@@ -13,8 +13,8 @@
                 <div class="main_home_slider text-center">
                     <div class="single_home_slider">
                         <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                            <h1>WELCOME TO OUR LOGIC</h1>
-                            <p>We Make Awesome Theme For Your Business</p>
+                            <h1>WELCOME TO PremierPesa</h1>
+                            <p>We transfer money internationaly</p>
                             <div class="home_btn">
                                 <a href="" class="btn btn-primary" style="border-radius: 25px; margin: 2px;">SIGN UP</a>
                             </div>
@@ -23,8 +23,8 @@
                     </div>
                     <div class="single_home_slider">
                         <div class="main_home wow fadeInUp" data-wow-duration="700ms">
-                            <h1>WELCOME TO OUR LOGIC</h1>
-                            <p>We Make Awesome Theme For Your Business</p>
+                            <h1>Its fast reliable and efficient</h1>
+                            <p>One of a kind</p>
                             <div class="home_btn">
                                 <a href="" class="btn btn-primary">LEARN MORE</a>
                             </div>
