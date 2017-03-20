0501b263c22a413b2be9f433076a12beb4abdc95
njerucyrus <njerucyrus5@hotmail.com>
2017-03-20 14:49:36 +0300

    completed review management admin

--- admin/manage_reviews_endpoint.php
+++ admin/manage_reviews_endpoint.php
@@ -5,25 +5,68 @@
  * Date: 3/20/17
  * Time: 9:52 AM
  */
+require_once __DIR__ . '/../models/class.feedback.php';
 
-require_once __DIR__.'/../models/class.feedback.php';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <?php include 'css.php';?>
-</head>
-<body>
-<div class="navbar navbar-inverse">
-    <?php include 'navbar.php'?>
-</div>
-<div class="container container-fluid">
-    <div class="row">
+if (isset($_POST['option'])) {
+    $option = $_POST['option'];
+    switch ($option) {
+        case 'approve_review':
+            approveReview();
+            break;
+        case 'delete_review':
+            deleteReview();
+            break;
 
+    }
+}
 
-    </div>
-</div>
+function approveReview()
+{
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
 
-<?php include 'js.php'?>
-</body>
-</html>
+        $approved = UserFeedback::approve($id);
+        if ($approved) {
+            print_r(json_encode(array(
+                "statusCode" => 200,
+                "message" => "Review approved successfully"
+            )));
+        } else {
+            print_r(json_encode(array(
+                "statusCode" => 500,
+                "message" => "Error occurred"
+            )));
+        }
+    } else {
+        print_r(json_encode(array(
+            "statusCode" => 500,
+            "message" => "Error! Could not find the review id"
+        )));
+    }
+}
+
+function deleteReview()
+{
+    if (isset($_POST['id'])) {
+        $id = $_POST['id'];
+
+        $deleted = UserFeedback::delete($id);
+        if ($deleted) {
+            print_r(json_encode(array(
+                "statusCode" => 200,
+                "message" => "Review approved successfully"
+            )));
+        } else {
+            print_r(json_encode(array(
+                "statusCode" => 500,
+                "message" => "Error occurred"
+            )));
+        }
+    } else {
+        print_r(json_encode(array(
+            "statusCode" => 500,
+            "message" => "Error! Could not find the review id"
+        )));
+    }
+}
+
