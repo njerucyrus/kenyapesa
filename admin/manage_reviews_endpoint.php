d1752acf8a54aef75f6aafbd45cf016c5d6c823d
njerucyrus <njerucyrus5@hotmail.com>
2017-03-20 10:09:20 +0300

    added reviews management

---
+++ admin/manage_reviews_endpoint.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hudutech
+ * Date: 3/20/17
+ * Time: 9:52 AM
+ */
+
+require_once __DIR__.'/../models/class.feedback.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include 'css.php';?>
+</head>
+<body>
+<div class="navbar navbar-inverse">
+    <?php include 'navbar.php'?>
+</div>
+<div class="container container-fluid">
+    <div class="row">
+
+
+    </div>
+</div>
+
+<?php include 'js.php'?>
+</body>
+</html>
