77bbc1ff8f1851f7df0d78235208befaeefa5f14
njerucyrus <njerucyrus5@hotmail.com>
2017-03-22 04:13:07 +0300

    added success page

--- views/success.php
+++ views/success.php
@@ -6,4 +6,20 @@
  * Time: 4:14 PM
  */
 
-echo "transaction was successfull";
+?>
+
+<div class="container container-fluid">
+    <div class="row">
+
+        <div class="col col-md-12">
+
+            <div class="alert alert-info">
+                <p>Your transaction was completed successfully.
+                    You will receive you Cash in you Mpesa Account in a moment.
+                </p>
+                <a href="#" class="btn btn-primary">Review our services here</a>
+            </div>
+
+        </div>
+    </div>
+</div>
